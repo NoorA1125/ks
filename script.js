6c3048000e447e0eae8507095c580fa12e7f61da
Noor Abdikadir <nooromar305@gmail.com>
2021-04-13 18:42:20 -0700

    Re-upload
    Installed files outside the main file

---
+++ script.js
@@ -0,0 +1,10 @@
+/*Navigation Scroll */
+var myNav = document.getElementsByName("nav");
+window.onscroll = function () {
+    // "use strict";
+    if (document.body.scrollTop >= 280 || document.documentElement.scrollTop >= 280) {
+        myNav.classList.add("scroll");
+    } else {
+        myNav.classList.remove("scroll");
+    }
+};
